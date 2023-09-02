4ad74995e587a23cac124a7a62b8941a752244a3
imCyberMohsin <mohsinansari7033@gmail.com>
2023-09-02 19:28:51 +0530

    static development done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //* Script - Modern Website 1 
+
+// Getting Elements
+const HoverElement = document.querySelector('.element');
+const hoverImg = document.querySelectorAll('.hover-img');
+
+const ShowImagesWhenHover = () =>{
+    HoverElement.addEventListener('mouseover', () => {
+        hoverImg.style.display = 'block';
+    })
+}
+
+// ShowImagesWhenHover();
